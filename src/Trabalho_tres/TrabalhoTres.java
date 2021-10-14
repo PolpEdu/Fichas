32d0f1277f4940e051f3aee639ed43883f1458f5
PolpErt <emailparaoedu@gmail.com>
2021-10-14 17:18:42 +0100

    Update TrabalhoTres.java

--- src/Trabalho_tres/TrabalhoTres.java
+++ src/Trabalho_tres/TrabalhoTres.java
@@ -6,16 +6,21 @@ import java.util.ArrayList;
 class Livro {
     private String titulo;
     private String nomeautor;
+    //private Requesicao requesicao;
 
     public Livro(String t, String autor) {
         this.titulo = t;
         this.nomeautor = autor;
     }
 
-    public static void listaLivrosDisponiveis(ArrayList<Livro> livros){ //getLivros
-        for(Livro i : livros) {
-            System.out.println("Livro:\nTitulo: "+i.titulo+"\nPor: "+i.nomeautor+"\n");
+    public static void listaLivrosDisponiveis(ArrayList<Livro> livros) { //getLivros
+        for (Livro i : livros) {
+            System.out.println("Livro:\nTitulo: " + i.titulo + "\nPor: " + i.nomeautor + "\n");
         }
+    }
+
+    public void addRequesicao(Requesicao req) {
+
     }
 }
 
@@ -28,9 +33,9 @@ class Leitor {
         this.nome = name;
     }
 
-    public static void listaLeitores(ArrayList<Leitor> leitores){ //getLivros
-        for(Leitor i : leitores) {
-            System.out.println("Leitor:\nNome: "+i.nome+" - "+i.nrutente+"\n");
+    public static void listaLeitores(ArrayList<Leitor> leitores) { //getLivros
+        for (Leitor i : leitores) {
+            System.out.println("Leitor:\nNome: " + i.nome + " - " + i.nrutente + "\n");
         }
     }
 }
@@ -38,15 +43,15 @@ class Leitor {
 class Data {
     private int dia, mes, ano;
 
-    public  Data(int d, int m, int a){ //construtor dá return a um objeto do tipo da classe
-        this.dia = d%32; //para nao ter dias maiores que 31
-        this.mes = m%13; //para nao ter meses maior que 13
+    public Data(int d, int m, int a) { //construtor dá return a um objeto do tipo da classe
+        this.dia = d % 32; //para nao ter dias maiores que 31
+        this.mes = m % 13; //para nao ter meses maior que 13
         this.ano = a;
         //dá return por si mesmo
     }
 
-    private String Data(){
-        return dia +"/"+mes+"/"+ano;
+    private String Data() {
+        return dia + "/" + mes + "/" + ano;
     }
 
     public String toString() {
@@ -54,36 +59,64 @@ class Data {
     }
 
 
-
-
     private Data dataDevolucao() {
-        return new Data(this.dia, (this.mes+1)%12, this.ano); //A data de devolucao é dada por o mes atual mais um.
+        return new Data(this.dia, (this.mes + 1) % 12, this.ano); //A data de devolucao é dada por o mes atual mais um.
     }
 
-    public Data getDataDev() { return dataDevolucao();}
+    public Data getDataDev() {
+        return dataDevolucao();
+    }
 }
+
 class Requesições {
-    ArrayList<Requesicao> req = new ArrayList<>();
+    private ArrayList<Requesicao> reqs;
+    private int nr;
+
+    public Requesições() {
+        reqs = new ArrayList<>();
+    }
+
+    public void imprimeReqs() {
+        for (Requesicao i : reqs) {
+            System.out.println("\nRequesição:" + i);
+        }
+    }
+
+    public void listaRequisitados(Data d) {
+        for (Requesicao req : reqs) {
+            if(req.comparadatas(d)) {
+                System.out.println(req);
+            }
+        }
+    }
 }
 
 
-class  Requesicao {
+class Requesicao {
     private Leitor leitor;
+    private Livro livro;
     private Data datarequesicao;
     private Data datadevolucao;
 
-    private Requesicao(Leitor l, Data dtreq, Data dtdev){
+    private Requesicao(Leitor l, Livro liv, Data dtreq, Data dtdev) {
         this.leitor = l;
-        this.datadevolucao=dtdev;
+        this.livro = liv;
+        this.datadevolucao = dtdev;
         this.datarequesicao = dtreq;
     }
 
-    public static Requesicao requesitalivro(Leitor l, Data dtReq, Data dtDev) {
-        return new Requesicao(l,dtReq,dtDev);
+    public static Requesicao requesitalivro(Leitor l, Livro liv,Data dtReq, Data dtDev) {
+        return  new Requesicao(l, liv, dtReq, dtDev);
     }
 
-    private void listaRequisitados(Data d){
+    public boolean comparadatas(Data datagiven){
+        return this.datarequesicao == datagiven;
+    }
 
+    private String Requesicao(){return "Leitor:"+leitor+"\nLivro: "+livro+"\nData de Requesição: "+datarequesicao+"\nData de Devolução: "+datadevolucao;}
+
+    public String toString(){
+        return Requesicao();
     }
 }
 
@@ -100,7 +133,7 @@ public class TrabalhoTres {
         leitores.add(l4);
     }
 
-    private static void criaLivros(ArrayList livros){
+    private static void criaLivros(ArrayList livros) {
         Livro l1 = new Livro("Os Maias", "Eça de Queiroz");
         Livro l2 = new Livro("O Ano Da Morte De Ricardo Reis", "José Saramago");
         Livro l3 = new Livro("Sherlock Holmes: A Study in Scarlet", "Arthur Conan Doyle");
@@ -109,8 +142,9 @@ public class TrabalhoTres {
         livros.add(l3);
     }
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         System.out.println("");
+        Requesições reqs = new Requesições();
         ArrayList<Leitor> leitores = new ArrayList<>(); //crio uma lista de leitores.
         ArrayList<Livro> livros = new ArrayList<>();
 
@@ -123,18 +157,18 @@ public class TrabalhoTres {
 
 
         //exemplo
-        Data d1 = new Data(23,2,2021);
+        Data d1 = new Data(23, 2, 2021);
         Data d1dev = d1.getDataDev();
 
-
-        Data now = new Data(14,12,2021);
+        Data now = new Data(14, 12, 2021);
         Data nowdev = now.getDataDev();
 
-        System.out.println("data: "+now+" - datadevolucao: "+nowdev);
+        System.out.println("data: " + now + " - datadevolucao: " + nowdev);
+
+        Livro livroareq = livros.get(2);
 
 
-        Requesicao.requesitalivro(leitores.get(0),now,nowdev);
-        Requesicao.requesitalivro(leitores.get(3),d1,d1dev);
+        Requesicao r = Requesicao.requesitalivro(leitores.get(3), livroareq,d1, d1dev);
 
     }
 }
