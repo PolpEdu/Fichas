fce4c263b3b10cef606974e1554b7af27b9847ef
Polpy <emailparaoedu@gmail.com>
2021-10-20 17:39:48 +0100

    update

--- src/Trabalho_tres/TrabalhoTres.java
+++ src/Trabalho_tres/TrabalhoTres.java
@@ -1,6 +1,7 @@
 package Trabalho_tres;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 class Livro {
@@ -10,12 +11,6 @@ class Livro {
     public Livro(String t, String autor) {
         this.titulo = t;
         this.nomeautor = autor;
-    }
-
-    public static void listaLivrosDisponiveis(ArrayList<Livro> livros) { //getLivros
-        for (Livro i : livros) {
-            System.out.println(i);
-        }
     }
 
     public Requesicao requesitaLivro(Leitor l, Data dtReq, Data dtDev) {
@@ -36,16 +31,73 @@ class Leitor {
         this.nome = name;
     }
 
-    public static void listaLeitores(ArrayList<Leitor> leitores) { //getLivros
-        for (Leitor i : leitores) {
-            System.out.println(i);
-        }
-    }
-
 
     private String PRINTLeitor(){return "Leitor: "+nome+", "+nrutente;}
 
     public String toString(){return PRINTLeitor();}
+}
+
+
+class Leitores {
+    private ArrayList<Leitor> leitores;
+
+    public void listaleitores() { //getLivros
+        for (Leitor i : this.leitores) {
+            System.out.println(i);
+        }
+    }
+
+    public void adicionaleitor(Leitor l){
+        this.leitores.add(l);
+    }
+
+    public Leitor getLeitor(int index) {
+        return this.leitores.get(index);
+    }
+
+    public void removeleitor(Leitor l) {
+        this.leitores.remove(l);
+    }
+
+    public boolean isEmpty() {
+        return leitores.isEmpty();
+    }
+
+    public ArrayList<Leitor> getLeitores(){
+        return this.leitores;
+    }
+
+}
+
+class Livros {
+    private ArrayList<Livro> livros;
+
+    public void listaLivrosDisponiveis() { //getLivros
+        for (Livro i : this.livros) {
+            System.out.println(i);
+        }
+    }
+
+    public void adicionalivro(Livro l){
+        this.livros.add(l);
+    }
+
+    public Livro getlivro(int index) {
+        return this.livros.get(index);
+    }
+
+    public void removelivro(Livro l) {
+        this.livros.remove(l);
+    }
+
+    public boolean isEmpty() {
+        return livros.isEmpty();
+    }
+
+    public ArrayList<Livro> getLivros(){
+        return this.livros;
+    }
+
 }
 
 class Data {
@@ -66,13 +118,40 @@ class Data {
         return PRINTData();
     }
 
+    public boolean comparadatas(Data datagiven){
+        //! NOTA: AS DATAS DE DEVOLUÇÃO SÃO UM MES DEPOIS.
+
+        // verfica se é do mes a seguir do proximo ano || verficia se é do mesmo mes || verfica se no caso de ser o mes a seguir, tem dias menores.
+        return ((this.ano == datagiven.ano-1) &&(this.mes == 12) && (datagiven.mes==1))
+                || (this.mes == datagiven.mes) && (this.ano == datagiven.ano) ||
+                (this.mes == (datagiven.mes+1)%13) && (this.dia <= datagiven.dia) && (this.ano == datagiven.ano);
+    }
 
     private Data dataDevolucao() {
+        //logica do calculo da dataDevolução:
+        //adicionar 1 ao ano no caso do mes ser 12
+        if(this.mes==12) return new Data(this.dia, (this.mes + 1) % 12, this.ano+1); //A data de devolucao é dada por o mes atual mais um.
+
         return new Data(this.dia, (this.mes + 1) % 12, this.ano); //A data de devolucao é dada por o mes atual mais um.
     }
 
     public Data getDataDev() {
         return dataDevolucao();
+    }
+
+    public static Data pedeData(Scanner stdin){
+        int dias, mes ,ano;
+        System.out.println("Seleciona a tua data: ");
+
+        System.out.print("Dia: ");
+        dias = stdin.nextInt();
+
+        System.out.print("Mes: ");
+        mes = stdin.nextInt();
+
+        System.out.print("Ano:");
+        ano = stdin.nextInt();
+        return new Data(dias,mes,ano);
     }
 }
 
@@ -83,31 +162,95 @@ class Requesicoes {
         reqs = new ArrayList<>();
     }
 
-
     public void imprimeReqs() {
         for (Requesicao i : reqs) {
             System.out.println("\nRequesição:\n" + i);
         }
     }
 
-    public void adicionareq(Requesicao r) {
+    public void adicionareq(Requesicao r, Livros livrosdisponiveis) {
+        //verificar se posso requesitar o livro pretendido (ver se ele já está requesitado)
         reqs.add(r);
+        //ja nao é disponivel
+        livrosdisponiveis.removelivro(r.getLivro());
     }
 
-
     public void listaRequisitados(Data d) {
-        System.out.print("\nLista de datas correspondidas a "+d);
+        System.out.print("\n---------------------\nLista de datas de livros requesitados em "+d);
         for (Requesicao req : reqs) {
-            if(req.comparadatas(d)) {
+            if(req.getDatarequesicao().comparadatas(d)) {
                 System.out.println("\n");
                 System.out.println(req);
             }
         }
+        System.out.println();
+    }
+
+    //esta funcao basicamente volta a por o livro nos livros disponiveis e remove a requesicao feita.
+    public void levantaLivro(Requesicao r,Livros livrosdisponiveis) {
+        if(reqs.contains(r))  {
+            reqs.remove(r);
+            System.out.println("Levantei o livro "+r.getLivro()+"\n");
+
+        }
+        livrosdisponiveis.adicionalivro(r.getLivro());
+    }
+
+    public Requesicao pedeRequesicao(Scanner stdin){
+        int i = 1;
+        for(Requesicao r: this.reqs){
+            System.out.println(i+"- Requesicao:\n"+r);
+            i++;
+        }
+        int escolher;
+        do  {
+            escolher = stdin.nextInt();
+        } while (!(escolher>0 &&escolher<=i));
+
+        // selecionei a requesicao vou dar-lhe return
+        return this.reqs.get(escolher-1);
+    }
+
+    public void criaReq(Scanner stdin, Livros ls, Leitores leitors){
+        int i = 1, escolherliv,j = 1, escolherleit;
+
+        if(ls.isEmpty() || leitors.isEmpty()) {
+            System.out.println("Não é possivel fazer requesições....");
+            return;
+        }
+        System.out.println("Seleciona o número do Livro: ");
+        for(Livro l: ls.getLivros()){
+            System.out.println(i+" - "+l);
+            i++;
+        }
+        do  {
+            escolherliv = stdin.nextInt();
+        } while (!(escolherliv>0 &&escolherliv<=i));
+
+
+        System.out.println("Seleciona o número do Leitor: ");
+
+        for(Leitor l: leitors.getLeitores()){
+            System.out.println(j+" - "+l);
+            j++;
+        }
+
+        do  {
+            escolherleit = stdin.nextInt();
+        } while (!(escolherleit>0 && escolherleit<=j));
+
+        Data datreq = Data.pedeData(stdin);
+        Data datadev = datreq.getDataDev();
+
+
+        //selececionei os leitores/livros/datareq adiciono as requesicoes.
+        Requesicao r = ls.getlivro(escolherliv-1).requesitaLivro(leitors.getLeitor(escolherleit-1),datreq,datadev);
+        this.adicionareq(r,ls);
+        System.out.println("Requesicao criada:\n"+r);
     }
 
 
 }
-
 
 class Requesicao {
     private Leitor leitor;
@@ -123,53 +266,92 @@ class Requesicao {
         this.datarequesicao = dtreq;
     }
 
+    public String toString(){return PRINTRequesicao();}
 
-    public boolean comparadatas(Data datagiven){
-        return this.datarequesicao == datagiven;
-    }
+    public Livro getLivro(){return this.livro;}
 
     private String PRINTRequesicao(){return leitor+"\n"+livro+"\n"+"Data de Requesição: "+ datarequesicao+"\n"+"Data de Devolução: "+datadevolucao;}
 
-    public String toString(){return PRINTRequesicao();}
+    public Data getDatarequesicao(){return this.datarequesicao;}
 }
 
 public class TrabalhoTres {
 
-    private static void criaLeitores(ArrayList<Leitor> leitores) {
+    private static void criaLeitores(Leitores leitores) {
         Leitor l1 = new Leitor("Eduardo Nunes", 2020217675);
         Leitor l2 = new Leitor("Francisco Rosendo", 2020217697);
         Leitor l3 = new Leitor("Afonso Gouveia Melo", 2020217360);
         Leitor l4 = new Leitor("Lucas Anjo", 2020218028);
-        leitores.add(l1);
-        leitores.add(l2);
-        leitores.add(l3);
-        leitores.add(l4);
+        leitores.adicionaleitor(l1);
+        leitores.adicionaleitor(l2);
+        leitores.adicionaleitor(l3);
+        leitores.adicionaleitor(l4);
     }
 
-    private static void criaLivros(ArrayList<Livro> livros) {
+    private static void criaLivros(Livros livros) {
         Livro l1 = new Livro("Os Maias", "Eça de Queiroz");
         Livro l2 = new Livro("O Ano Da Morte De Ricardo Reis", "José Saramago");
         Livro l3 = new Livro("Sherlock Holmes: A Study in Scarlet", "Arthur Conan Doyle");
-        livros.add(l1);
-        livros.add(l2);
-        livros.add(l3);
+        Livro l4 = new Livro("Engenharia de Janelas", "João Barata");
+        Livro l5 = new Livro("Os Lusiadas", "Luis Vaz de Camões");
+
+        livros.adicionalivro(l1);
+        livros.adicionalivro(l2);
+        livros.adicionalivro(l3);
+        livros.adicionalivro(l4);
+        livros.adicionalivro(l5);
     }
+
+    private static Leitor criaLeitorInput(Leitores leitores, Scanner stdin) {
+        String nome, nr;
+        long nrLONG;
+
+        System.out.print("Nome: ");
+        nome = stdin.nextLine();
+
+        System.out.print("Numero: ");
+        nr = stdin.nextLine();
+
+        Leitor leit = null;
+        try {
+            nrLONG = Long.parseLong(nr);
+            leit = new Leitor(nome,nrLONG);
+            leitores.adicionaleitor(leit);
+            System.out.print(leit);
+
+
+        }catch (Exception e) {
+            System.out.println("Número de estudante invalido. Erro: "+e);
+        }
+        return leit;
+    }
+
+    private static Livro criaLivroInput(Livros livros,Scanner stdin) {
+        String t, autor;
+        t = stdin.nextLine(); //para fazer o paragrafo e esperar
+
+        System.out.print("Título: ");
+        t = stdin.nextLine();
+
+        System.out.print("Autor: ");
+        autor = stdin.nextLine();
+
+        Livro l = new Livro(t,autor);
+        livros.adicionalivro(l);
+        System.out.print(l);
+        return l;
+    }
+
 
     public static void main(String[] args) {
         Requesicoes listareqs = new Requesicoes();
-        ArrayList<Leitor> leitores = new ArrayList<>(); //crio uma lista de leitores.
-        ArrayList<Livro> livros = new ArrayList<>();
-
-        //criação de dados
-        criaLeitores(leitores);
-        criaLivros(livros);
+        Leitores listaleitores = new Leitores();
+        Livros listalivros = new Livros();
 
 
-        //lista todos os dados disponiveis
-        System.out.println("Livros disponiveis:");
-        Livro.listaLivrosDisponiveis(livros);
-        System.out.println("\nLeitores disponiveis:");
-        Leitor.listaLeitores(leitores);
+        //criação de dados iniciais
+        criaLeitores(listaleitores);
+        criaLivros(listalivros);
 
 
         //exemplo de datas e as suas devoluções
@@ -179,21 +361,72 @@ public class TrabalhoTres {
         Data now = new Data(14, 12, 2021);
         Data nowdev = now.getDataDev();
 
+        /*Data antesnow = new Data(12, 1, 2021);
+        Data antesnowdev = antesnow.getDataDev();*/
 
-        //cria requesições:
-        Requesicao r = livros.get(2).requesitaLivro(leitores.get(2),d1, d1dev);
-        Requesicao r1 = livros.get(1).requesitaLivro(leitores.get(0), now, nowdev);
-        Requesicao r2 =  livros.get(0).requesitaLivro(leitores.get(1), now, nowdev);
 
-        //adiciona às requesições
-        listareqs.adicionareq(r);
-        listareqs.adicionareq(r1);
-        listareqs.adicionareq(r2);
+        //exemplos de criação de requesições:
+        Requesicao r = listalivros.getlivro(2).requesitaLivro(listaleitores.getLeitor(2),d1, d1dev);
+        Requesicao r1 = listalivros.getlivro(1).requesitaLivro(listaleitores.getLeitor(0), now, nowdev);
+        // Requesicao r2 =  livrosdisponiveis.get(0).requesitaLivro(leitores.get(1), now, nowdev);
+        // Requesicao r3 =  livrosdisponiveis.get(4).requesitaLivro(leitores.get(1), antesnow, antesnowdev);
 
-        //lista todas as requisições:
-        listareqs.imprimeReqs();
 
-        //lista todas as requisições com base numa data dada.
-        listareqs.listaRequisitados(now);
+        //adiciona às requesições algumas requesicoes posteriores.
+        listareqs.adicionareq(r,listalivros);
+        listareqs.adicionareq(r1,listalivros);
+
+
+        int escolha;
+        Scanner stdin = new Scanner(System.in);
+        do {// Menu
+            System.out.println("\n----------------------------\n1 - Adicionar livro");
+            System.out.println("2 - Adicionar Leitor");
+            System.out.println("3 - Lista livros disponiveis para requesicao");
+            System.out.println("4 - Lista leitores");
+            System.out.println("5 - Lista Reposicoes");
+            System.out.println("6 - Cria uma reposição");
+            System.out.println("7 - Lista livros requesitados com base numa data pedida");
+            System.out.println("8 - Levanta Livro");
+
+            System.out.println("0 - Sair\n----------------------------");
+            escolha = stdin.nextInt();
+            switch (escolha) {
+                case 1:
+                    criaLivroInput(listalivros,stdin);
+                    break;
+                case 2:
+                    stdin.nextLine(); //para saltar a proxima linha
+                    criaLeitorInput(listaleitores,stdin);
+                    break;
+                case 3:
+                    listalivros.listaLivrosDisponiveis();
+                    break;
+                case 4:
+                    listaleitores.listaleitores();
+                    break;
+                case 5:
+                    listareqs.imprimeReqs();
+                    break;
+                case 6:
+                    listareqs.criaReq(stdin,listalivros,listaleitores);
+                    break;
+                case 7:
+                    Data ped = Data.pedeData(stdin);
+                    //lista todas as requisições com base numa data dada.
+                    listareqs.listaRequisitados(ped);
+                    break;
+                case 8:
+                    Requesicao reqped = listareqs.pedeRequesicao(stdin);
+                    listareqs.levantaLivro(reqped, listalivros);
+                    break;
+                default:
+                    System.out.println("Input invalido.");
+                    break;
+                case 0:
+                    System.exit(0);
+            }
+        } while (escolha != 0);
+        stdin.close();
     }
 }
